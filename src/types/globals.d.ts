5a4398492e24fd0498e21fd423706fba1b955dc8
NativeCode <tm.nicolasmachuca@gmail.com>
2026-05-20 12:59:59 -0400

    Usuarios clerk

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -3,7 +3,7 @@ export {};
 declare global {
   interface CustomJwtSessionClaims {
     metadata: {
-      role?: "default" | "admin" | "recepcionista" | "secretaria" | "basico" | "clinico-medico" | "odontologico" | "agenda" | "configuracion";
+      role?: "default" | "admin" | "recepcionista" | "secretaria" | "basico" | "centro-estetico" | "clinico-medico" | "odontologico" | "oftalmologia" | "agenda" | "configuracion";
     };
   }
 }
